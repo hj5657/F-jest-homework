c988894ffa2525cb60a33d22f5ef9a58e6f68c0a
hejie <565703319@qq.com>
2020-11-15 12:03:03 +0800

    [hj] finish expect calls test

--- src/matchers/__test__/expect.calls.test.js
+++ src/matchers/__test__/expect.calls.test.js
@@ -2,23 +2,23 @@ function forEach(items, callback) {
   items.forEach((item) => callback(item));
 }
 
-test("TODO 11", () => {
+test('TODO 11', () => {
   const mockCallback = jest.fn((x) => 42 + x);
   forEach([1, 2], mockCallback);
-
   // TODO 11: add assertion
+  expect(mockCallback).toHaveBeenCalledTimes(2);
 });
 
-test("TODO 12", () => {
+test('TODO 12', () => {
   const mockFn = jest.fn();
   mockFn
     .mockReturnValueOnce(42)
-    .mockReturnValueOnce("string")
+    .mockReturnValueOnce('string')
     .mockReturnValue(true);
 
   // TODO 12: to add "expected" value
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
-  expect(mockFn()).toBe(expected);
+  expect(mockFn()).toBe(42);
+  expect(mockFn()).toBe('string');
+  expect(mockFn()).toBe(true);
+  expect(mockFn()).toBe(true);
 });
